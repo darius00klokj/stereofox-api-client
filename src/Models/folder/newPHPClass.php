972bba984b159f42104a634a8334e39aec98c734
Darius Mac 2023 <darius00klokj@gmail.com>
2023-04-27 11:13:49 +0200

    Testing

---
+++ src/Models/folder/newPHPClass.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+/**
+ * Description of newPHPClass
+ *
+ * @author darius
+ */
+class newPHPClass {
+    //put your code here
+}
