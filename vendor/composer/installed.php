d477edc12adb51aed52f41b787660c0a409d35a5
Darius Mac 2023 <darius00klokj@gmail.com>
2024-08-19 12:38:57 +0200

    Adding params

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'darius00klokj/stereofox-api-client',
+        'pretty_version' => '2.0',
+        'version' => '2.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'darius00klokj/stereofox-api-client' => array(
+            'pretty_version' => '2.0',
+            'version' => '2.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
